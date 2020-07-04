0b79f4a2c3fd7415b7b661b776c72b5ae027fa86
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-07-04 14:34:33 +0300

    I created new elements

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 console.log('it works');
 
 // code your solution in here
+//Here we will create all elements we need
+const newPostList = document.getElementById('post-list');
+const newSpaceDiv = document.createElement('div');
+const newBodyDiv = document.createElement('div');
+const newTitle = document.createElement('h5');
+const newParagraph = document.createElement('p');
+
+//Then we append them to their parents
+newPostList.appendChild(newSpaceDiv);
+newSpaceDiv.appendChild(newBodyDiv);
+newBodyDiv.appendChild(newTitle);
+newBodyDiv.appendChild(newParagraph);
+
+// We will give them classes
+newSpaceDiv.classList.add('some-space');
+newBodyDiv.classList.add('card-body');
+newTitle.classList.add('card-title');
+newParagraph.classList.add('card-text');
